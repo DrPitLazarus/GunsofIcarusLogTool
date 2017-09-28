0cac3eaa558e054b9ef5b8b61edb58380ec8ef3b
Dr. Pit Lazarus <drpitlazarus@gmail.com>
2017-09-27 21:40:29 -0400

    Forgot to comment out the right BaseDirectory

--- GoILogWatchdog/App.cs
+++ GoILogWatchdog/App.cs
@@ -9,8 +9,8 @@ namespace GoILogWatchdog
 {
     public static class App
     {
-        public static readonly string BaseDirectory = @"C:\Steam\steamapps\common\Guns of Icarus Online\GoILogWatchdog\";
-        //public static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        //public static readonly string BaseDirectory = @"C:\Steam\steamapps\common\Guns of Icarus Online\GoILogWatchdog\";
+        public static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         public static readonly string GoIDirectory = Path.GetFullPath(Path.Combine(BaseDirectory, @"..\"));
         public static readonly string OutputLogPath = @"GunsOfIcarusOnline_data\output_log.txt";
         public static readonly string Title = "GoI Log Watchdog";
